073b19c64d7aa356baff21d60a94e1f8f4d50297
Lince Potiguara <12141500+LincePotiguara@users.noreply.github.com>
2022-03-10 18:08:10 -0300

    centraliza a div

--- src/main/java/br/ufrj/ic/campominado/ContinuarResource.java
+++ src/main/java/br/ufrj/ic/campominado/ContinuarResource.java
@@ -1,6 +1,7 @@
 package br.ufrj.ic.campominado;
 
 import javax.ws.rs.*;
+import java.util.Locale;
 
 @Path("continuar")
 public class ContinuarResource {
@@ -14,13 +15,25 @@ public class ContinuarResource {
             case "15x15": linha = 15; break;
             case "20x20": linha = 20; break;
         }
+
+        tabuleiro.append("<div style=\"background-color: blue;display: flex;\">");
         for(int i = 0; i < linha; i++) {
-            tabuleiro.append("#");
+            tabuleiro.append("<div class='quadrado'>#</div>");
+            //tabuleiro.append("<td><div class='cell-content'>#</div></td>");
         }
-        String temp = tabuleiro.append("<br>").toString();
+        tabuleiro.append("</div>");
+        //String temp = tabuleiro.append("<br>").toString();
+        String temp = tabuleiro.toString();
         for(int i = 0; i < linha; i++) {
             tabuleiro.append(temp);
         }
+        //tabuleiro.insert(0,"<table id='table-data' cellpadding=0 cellspacing=0>");
+        //tabuleiro.append("</table>");
+        // Essa função cria o campo. Modifique a classe quadrado e o style inline para mudar o estilo
+        // |
+        // V
+        tabuleiro.insert(0, "<div style=\"display: inline-block;\">");
+        tabuleiro.append("</div>");
         return tabuleiro.toString();
     }
 }
--- src/main/java/br/ufrj/ic/campominado/DificuldadeResource.java
+++ src/main/java/br/ufrj/ic/campominado/DificuldadeResource.java
@@ -11,9 +11,9 @@ public class DificuldadeResource {
     public String dificuldade() {
         return "<center>"
                 + "<p><h2>Selecione o tamanho do tabuleiro</h2></p>"
-                + "<p><a onclick=\"ApiResponse('novo jogo', '10x10')\">Pequeno (10x10)</a></p>"
-                + "<p><a onclick=\"ApiResponse('novo jogo', '15x15')\">Médio (15x15)</a></p>"
-                + "<p><a onclick=\"ApiResponse('novo jogo', '20x20')\">Grande (20x20)</a></p>"
+                + "<p><a onclick=\"ApiResponse('novo jogo', '10x10')\" class='hover-text' >Pequeno (10x10)</a></p>"
+                + "<p><a onclick=\"ApiResponse('novo jogo', '15x15')\" class='hover-text' >Médio (15x15)</a></p>"
+                + "<p><a onclick=\"ApiResponse('novo jogo', '20x20')\" class='hover-text' >Grande (20x20)</a></p>"
                 + "</center>";
     }
 }
